f029fa3bfec520cc26e9a9bbb58e2a756d8e5120
cesar huerta <cesarhuertaescalante@hotmail.com>
2018-03-14 02:44:54 -0600

    Registros/administrador2.0

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -15,4 +15,49 @@ class userController extends Controller
 
     	return view('alumnos')->with('estudiantes', $users);
     }
+
+
+    public function create()
+	{
+		return view('test.createUsers');
+
+	}
+	public function store(userrsFromRequest $request)
+	{
+		$user = new User;
+		$user->nombre=$request->get('nombre');
+		$user->apellidoM=$request->get('apellidoM');
+		$user->apellidoP=$request->get('apellidoP');
+		$user->email=$request->get('email');
+		$user->password=$request->get('password');
+		$user->save();
+
+		return Redirect::to('test');
+	}
+	public function show($id)
+	{
+		return view('test.show',['user'=>user::findOrFail($id)]);
+	}
+	public function edit($id)
+	{
+		return view('test.edit',['user'=>user::findOrFail($id)]);
+	}
+	public function update(userrsFromRequest $request,$id)
+	{
+		$user = User::findOrFail($id);
+		$user->nombre=$request->get('nombre');
+		$user->apellidoM=$request->get('apellidoM');
+		$user->apellidoP=$request->get('apellidoP');
+		$user->email=$request->get('email');
+		$user->password=$request->get('password');
+		$user->update();
+
+		return Redirect::to('test');
+	}
+	public function destroy($id)
+	{
+		$user = User::findOrFail($id);
+		$user->update();
+		return Redirect::to('test');
+	}
 }
