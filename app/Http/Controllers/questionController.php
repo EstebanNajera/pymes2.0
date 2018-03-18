ca939e2b85bd3fda96d89e11dfcd69acdb9962ae
Erik Alvarez <erikalvarez00@gmail.com>
2018-03-17 19:16:41 -0600

    Ajustes varios, se puede insertar preguntas :)

--- app/Http/Controllers/questionController.php
+++ app/Http/Controllers/questionController.php
@@ -2,11 +2,105 @@
 
 namespace App\Http\Controllers;
 
+use App\Area;
+
+use App\Category;
+
+use App\Question;
+
 use Illuminate\Http\Request;
 
 class questionController extends Controller
 {
-	public function create(){
-		return view("createQuestions");
-	}
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // Obtener todas las categorias
+        $categories = Category::all();
+
+        // Obtener todas las areas con al menos una categoría
+        $areas = Area::join('categories', 'areas.id_area', '=', 'categories.id_area')
+            ->select('areas.area', 'areas.id_area')
+            ->distinct()
+            ->get();
+
+        return view('createQuestions', compact('areas', 'categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Question::create([
+            'id_category' => $request -> id_category,
+            'question' => $request -> question,
+            'type' => $request -> type,
+            'status' => ($request -> status == null) ? 0:1
+        ]);
+
+        return view('Home');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
--- resources/views/createQuestions.blade.php
+++ resources/views/createQuestions.blade.php
@@ -3,7 +3,8 @@
 @section('content')
     <h1 class="title">Preguntas</h1>
     <div class="offset-md-3 col-md-6 offset-md-3">
-	    <form method="POST">
+	    <form method="POST" action="{{ route('questions.store') }}">
+	    	{{ csrf_field() }}
 			<div class="card">
 				<div class="card-title"><b>Insertar pregunta</b></div>
 				<div class="card-body">
@@ -11,19 +12,19 @@
 						<label>Pregunta</label>
 					</div>
 					<div class="row">
-						<input class="form-control" type="text" name="respuesta" placeholder="Introduzca su pregunta aquí">
+						<input class="form-control" type="text" name="question" placeholder="Introduzca su pregunta aquí">
 					</div>
 					<div class="row">
 						<label>Tipo de pregunta</label>
 					</div>
 					<div class="row text-center">
-					    <label class="col-md-3 radio-inline"><input type="radio" name="tipo">Abierta</label>
-					    <label class="col-md-4 radio-inline"><input type="radio" name="tipo">Cerrada simple</label>
-				        <label class="col-md-5 radio-inline"><input type="radio" name="tipo" checked>Cerrada de opción múltiple</label>
+					    <label class="col-md-3 radio-inline"><input type="radio" name="type" value="2" checked>Abierta</label>
+					    <label class="col-md-4 radio-inline"><input type="radio" name="type" value="1">Cerrada simple</label>
+				        <label class="col-md-5 radio-inline"><input type="radio" name="type" value="3">Cerrada de opción múltiple</label>
 					</div>
-					<div class="row">
+					{{-- <div class="row">
 						<label>Incisos</label>
-					</div>
+					</div> --}}
 					
 					{{-- Aquí puedes añadir los incisos --}}
 
@@ -31,12 +32,10 @@
 						<div class="col-md-6">
 							<div class="form-group">
 								<label for="sel1">Area</label>
-								<select class="form-control" id="sel1">
-									<option>Área 1</option>
-									<option>Área 2</option>
-									<option>Área 3</option>
-									<option>Área 4</option>
-									<option>Área 5</option>
+								<select class="form-control" name="id_area">
+									@foreach($areas as $area)
+									    <option value={{ $area -> id_area }}>{{ $area -> area }}</option>
+									@endforeach
 								</select>
 							</div>
 						</div>
@@ -44,22 +43,20 @@
 						<div class="col-md-6">
 							<div class="form-group">
 								<label for="sel1">Categoría</label>
-								<select class="form-control" id="sel1">
-									<option>Categoría 1</option>
-									<option>Categoría 2</option>
-									<option>Categoría 3</option>
-									<option>Categoría 4</option>
-									<option>Categoría 5</option>
+								<select class="form-control" name="id_category">
+									@foreach($categories as $category)
+									    <option value={{ $category -> id_category }}>{{ $category -> category }}</option>
+									@endforeach
 								</select>
 							</div>
 						</div>
 				    </div>
 					<div class="row text-center">
-					    <label class="checkbox-inline"><input type="checkbox" name="tipo" checked>Activa</label>
+					    <label class="checkbox-inline"><input type="checkbox" name="status" value="1" checked>Activa</label>
 					</div>
 					<div class="row">
-		                <span class="input-group-btn">
-		    	            <imput type="submit" class="btn btn-success">Aceptar</button>
+		                <span class="input-group-btn text-center col-md-4 offset-md-4">
+		    	            <input type="submit" class="btn btn-success" value="Guardar"></input>
 		                </span>
 					</div>
 				</div>
